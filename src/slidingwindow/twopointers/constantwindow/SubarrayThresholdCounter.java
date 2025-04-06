388657107217b55d803b10b20945091bceed5f1b
naveennk045 <naveennk045@gmail.com>
2025-04-06 16:58:47 +0530

    feat(dsa): solve Day 5 problems - subarray , substrings using two pointers and sliding window

---
+++ src/slidingwindow/twopointers/constantwindow/SubarrayThresholdCounter.java
@@ -0,0 +1,28 @@
+package slidingwindow.twopointers.constantwindow;
+
+// https://leetcode.com/problems/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/description/
+
+public class SubarrayThresholdCounter {
+    public static int numOfSubArrays(int[] arr, int k, int threshold) {
+
+        int count = 0;
+        int sum = 0;
+        for (int i = 0; i < k; i++){
+            sum+= arr[i];
+        }
+
+        if (sum/k >= threshold) count++;
+
+        for (int right = k; right < arr.length; right++){
+            sum+= arr[right] - arr[right-k];
+            if(sum/k >= threshold) count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3 , 4, 6};
+        System.out.println(numOfSubArrays(nums,3,7));
+    }
+}
