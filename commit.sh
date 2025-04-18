3d6fd66cc0b9942027e29a38244571a32d858871
Naveenkumar <naveennk045@gmail.com>
2025-04-18 10:14:20 +0530

    feat(dsa): solve  problems - subarray , substrings using two pointers , sliding window and prefix sum

--- commit.sh
+++ commit.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# Get the last recorded day from commit messages
-last_day=$(git log --pretty=format:"%s" | grep -o 'Day [0-9]*' | head -n 1 | awk '{print $2}')
-
-# If no previous day is found, start from 1
-if [ -z "$last_day" ]; then
-    next_day=1
-else
-    next_day=$((last_day + 1))
-fi
-
 # Define the commit message
-commit_message="feat(dsa): solve Day $next_day problems - subarray , substrings using two pointers and sliding window"
+commit_message="feat(dsa): solve  problems - subarray , substrings using two pointers , sliding window and prefix sum"
 
 # Add changes, commit, and push
 git add .
