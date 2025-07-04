6c37e7361e04943ac87fb7615a55eab8604df477
Naveenkumar <naveennk045@gmail.com>
2025-07-04 11:06:33 +0530

    Recursion problems

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -3,35 +3,37 @@ package sorting;
 import java.util.Arrays;
 
 public class QuickSort {
-    public static void sort(int[] nums,int low,int high){
+    public static void sort(int[] nums, int low, int high) {
 
-        if(low>=high) return;
+        if (low >= high) return;
 
-        int start=low;
-        int end=high;
+        int start = low;
+        int end = high;
 
-        int mid=start+(end-start)/2;
-        int pivot=nums[mid];
+        int mid = start + (end - start) / 2;
+        int pivot = nums[mid];
 
-        while(start<=end){
-            while(nums[start]<pivot){
+        while (start <= end) {
+            while (nums[start] < pivot) {
                 start++;
-            }while(nums[end]>pivot){
+            }
+            while (nums[end] > pivot) {
                 end--;
             }
-           if(start<=end) {
-               Swap.swap(nums,start,end);
-               start++;
-               end--;
-           }
+            if (start <= end) {
+                Swap.swap(nums, start, end);
+                start++;
+                end--;
+            }
         }
 
-        sort(nums,low,end);
-        sort(nums,start,high);
+        sort(nums, low, end);
+        sort(nums, start, high);
     }
+
     public static void main(String[] args) {
-        int[]  nums={0,9,3,1,2,7,6,4,10,5,8};
-        sort(nums,0,nums.length-1);
+        int[] nums = {0, 9, 3, 1, 2, 7, 6, 4, 10, 5, 8};
+        sort(nums, 0, nums.length - 1);
         System.out.println(Arrays.toString(nums));
 
     }
