ef89118015d10d02a3f0f53e5ae91c860d34566b
Naveenkumar <naveennk045@gmail.com>
2025-07-24 05:55:44 +0530

    Working on recursion tree

---
+++ src/greedy/RefuelingStops.java
@@ -0,0 +1,18 @@
+package greedy;
+
+
+public class RefuelingStops {
+    public static int minRefuelStops(int target, int startFuel, int[][] stations) {
+
+        int kmTravelled = 0;
+        int fuelTank = 0;
+
+        return 0;
+
+            
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
