5f525159d0072a3aa10e1a5fa7e37b58c16cda7d
naveennk045 <naveennk045@gmail.com>
2025-09-15 14:52:02 +0530

    feat : add logic for rotation for balancing the tree

--- src/tree/avl/AVLTree.java
+++ src/tree/avl/AVLTree.java
@@ -34,9 +34,71 @@ public class AVLTree {
         } else {
             root.right = insertHelper(value, root.right);
         }
-
         root.height = calculateHeight(root);
+
+        return rotate(root);
+    }
+
+    private AVLTreeNode rotate(AVLTreeNode root) {
+        int balanceFactor = calculateBalanceFactor(root);
+
+        // Case 1 : for leaf heavy
+        if (balanceFactor > 1) {
+            // Left - Left case :
+            if (calculateBalanceFactor(root.left) >= 0) {
+                return rightRotate(root);
+            } else {
+                root.left = leftRotate(root.left);
+                return rightRotate(root);
+            }
+
+        }
+        // Case 2 : for right heavy
+        else if (balanceFactor < -1) {
+            if (calculateBalanceFactor(root.right) <= 0) {
+                return leftRotate(root);
+            } else {
+                root.right = rightRotate(root.right);
+                return leftRotate(root);
+            }
+        }
         return root;
+
+    }
+
+    private AVLTreeNode leftRotate(AVLTreeNode x) {
+        AVLTreeNode y = x.right;
+        AVLTreeNode t2 = y.left;
+
+        y.left = x;
+        x.right = t2;
+
+        x.height = calculateHeight(x);
+        y.height = calculateHeight(y);
+
+        return y;
+    }
+
+    private AVLTreeNode rightRotate(AVLTreeNode y) {
+        AVLTreeNode x = y.left;
+        AVLTreeNode t2 = x.right;
+
+        x.right = y;
+        y.left = t2;
+
+        y.height = calculateHeight(y);
+        x.height = calculateHeight(x);
+
+        return x;
+    }
+
+    private int calculateBalanceFactor(AVLTreeNode root) {
+        if (root == null) return 0;
+
+        int leftHeight = root.left == null ? -1 : root.left.height;
+        int rightHeight = root.right == null ? -1 : root.right.height;
+        return leftHeight - rightHeight;
+
     }
 
     // for testing whether insertion is correct or not
@@ -55,7 +117,9 @@ public class AVLTree {
 
     private int calculateHeight(AVLTreeNode node) {
         if (node == null) return -1;
-        return 1 + Math.max(calculateHeight(node.left), calculateHeight(node.right));
+        int leftHeight = (node.left != null) ? node.left.height : -1;
+        int rightHeight = (node.right != null) ? node.right.height : -1;
+        return 1 + Math.max(leftHeight, rightHeight);
     }
 
     public void display() {
