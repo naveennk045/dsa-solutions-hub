0bcd1e59d6d3c748c82def451b5e2de391861706
naveennk045 <naveennk045@gmail.com>
2025-09-20 16:24:09 +0530

    feat(Leetcode) : Split arrays into k subarrays

---
+++ src/arrays/SplitArrayIntoKSubarrays.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class SplitArrayIntoKSubarrays {
+
+    static class Data{
+        int sum;
+        int start;
+        int end;
+    }
+    public static int splitArray(int[] nums, int k) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
