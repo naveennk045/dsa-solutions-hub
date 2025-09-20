dccd45aa6c6ace7fdb1bc26bc0ab0aa15ba95a81
naveennk045 <naveennk045@gmail.com>
2025-09-20 16:40:20 +0530

    feat(leetcode) : working on interleaving strings problem

--- src/dynamicprogramming/InterleavingStrings.java
+++ src/dynamicprogramming/InterleavingStrings.java
@@ -1,4 +1,26 @@
 package dynamicprogramming;
 
 public class InterleavingStrings {
+
+    public static boolean isInterleave(String s1, String s2, String s3) {
+        return generatePermutation(s1 + s2, s3, "", new boolean[s1.length() + s2.length()]);
+    }
+
+    public static boolean generatePermutation(String str, String target, String currPermutation, boolean[] used) {
+        if (currPermutation.length() == target.length()) {
+            System.out.println(currPermutation);
+            return currPermutation.equals(target);
+        }
+        for (int i = 0; i < used.length; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            if (generatePermutation(str, target, currPermutation + str.charAt(i), used)) return true;
+            used[i] = false;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isInterleave("aabcc", "dbbca", "aadbbcbcac"));
+    }
 }
