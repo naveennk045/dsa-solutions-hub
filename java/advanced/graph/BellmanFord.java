ea83aa9ed51e6fddab27b043e6176162cbeec5e5
naveennk045 <naveennk045@gmail.com>
2025-11-30 17:11:31 +0530

    feat(graph): add BellmanFord

---
+++ java/advanced/graph/BellmanFord.java
@@ -0,0 +1,30 @@
+package advanced.graph;
+
+import java.util.Arrays;
+
+public class BellmanFord {
+
+    public int[] bellmanFord(int V, int[][] edges, int src) {
+
+        int[] distance = new int[V];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[src] = 0;
+
+        /*
+        First we need to write the edges, and we need to find the shortest path for v times
+        Right now, I have idea to find the shortest path using bfs , run loop for another v times
+        If there is the negative cycle edge means we cannot be able to find the shortest path for all.
+        */
+
+        /*
+        Before that I need to know about
+        * */
+
+
+        return distance;
+    }
+
+    static void main() {
+
+    }
+}
