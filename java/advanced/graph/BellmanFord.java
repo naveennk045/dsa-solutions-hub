e89fa38462e658be77daee183b041b6e418fc71b
naveennk045 <naveennk045@gmail.com>
2025-12-02 10:53:46 +0530

    problem(leetcode): Bellmann ford algorithm

--- java/advanced/graph/BellmanFord.java
+++ java/advanced/graph/BellmanFord.java
@@ -1,23 +1,39 @@
 package advanced.graph;
 
-import java.util.Arrays;
+import java.util.*;
+
 
 public class BellmanFord {
 
     public int[] bellmanFord(int V, int[][] edges, int src) {
 
+        int MAX_VALUE =  (int)Math.pow(10,8);
         int[] distance = new int[V];
-        Arrays.fill(distance, Integer.MAX_VALUE);
+        Arrays.fill(distance,MAX_VALUE);
         distance[src] = 0;
 
-        /*
-        First we need to write the edges, and we need to find the shortest path for v times
-        Right now, I have idea to find the shortest path using bfs , run loop for another v times
-        If there is the negative cycle edge means we cannot be able to find the shortest path for all.
-        */
+        for (int i = 0; i < V - 1; i++) {
+            for (int[] e : edges) {
+                int u = e[0];
+                int v = e[1];
+                int w = e[2];
 
+                if (distance[u] != MAX_VALUE
+                        && distance[u] + w < distance[v]) {
+                    distance[v] = distance[u] + w;
+                }
+            }
+        }
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
 
-
+            if (distance[u] != MAX_VALUE
+                    && distance[u] + w < distance[v]) {
+                return new int[]{-1};
+            }
+        }
         return distance;
     }
 
